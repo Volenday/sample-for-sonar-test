87839087962c7b29c39521e7e366287a1e3cf55b
Dave Limutin <dave.limutin@volenday.com>
2024-11-21 15:42:13 +0800

    test

--- app/page.js
+++ app/page.js
@@ -12,12 +12,12 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol style={{backgroundColor: 'red !important'}} className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.js
-            </code>
+            </code>>
             .
           </li>
           <li>Save and see your changes instantly.</li>
