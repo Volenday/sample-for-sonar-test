07404c15740d0b5d5e5a4d1648372b8d5743d1c3
Dave Limutin <dave.limutin@volenday.com>
2024-11-22 18:09:44 +0800

    testing sonar on vercel

--- sonar.js
+++ sonar.js
@@ -1,20 +1,17 @@
-// const sonarqubeScanner = require('sonarqube-scanner');
-
-// sonarqubeScanner(
-//   {
-//     serverUrl: 'https://sonarcloud.io', // or your SonarQube server URL
-//     token: process.env.SONAR_TOKEN,
-//     options: {
-//       'sonar.projectKey': 'Volenday_sample-for-sonar-test',
-//       'sonar.organization': 'volenday', // Only for SonarCloud
-//       'sonar.sources': '.', // Analyze the current directory
-//       'sonar.exclusions': 'node_modules/**,.next/**,public/**,.github/**', // Exclude unnecessary directories
-//     //   'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info', // If using test coverage
-//       'sonar.sourceEncoding': 'UTF-8',
-//     },
-//   },
-//   () => process.exit()
-// );
 const sonarqubeScanner = require('sonarqube-scanner');
 
-console.log('✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅', sonarqubeScanner);
+// sonarqubeScanner.default(
+sonarqubeScanner.scan(
+  {
+    serverUrl: 'https://sonarcloud.io', // or your SonarQube server URL
+    token: process.env.SONAR_TOKEN,
+    options: {
+      'sonar.projectKey': 'Volenday_sample-for-sonar-test',
+      'sonar.organization': 'volenday', // Only for SonarCloud
+      'sonar.sources': '.', // Analyze the current directory
+      'sonar.exclusions': 'node_modules/**,.next/**,public/**,.github/**', // Exclude unnecessary directories
+      'sonar.sourceEncoding': 'UTF-8',
+    },
+  },
+  () => process.exit()
+);
