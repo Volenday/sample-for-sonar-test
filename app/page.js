77c70dcd67591a318f9e35d73f55862f041c3db5
Dave Limutin <dave.limutin@volenday.com>
2024-11-22 12:50:09 +0800

    testing how vercel works regarding its deployment process and flow

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
 // const password = 'asdasdasdasdsada';
-const ahamaticServiceAccount = {
-  apiKey: '881c0820-7a7a-11ec-b207-8dd119227be1',
-  emailAddress: 'developers@volenday.com',
-  password: 'V0l3nd@yP@ssw0rd',
-  rememberMe: true
-}
+// const ahamaticServiceAccount = {
+//   apiKey: '881c0820-7a7a-11ec-b207-8dd119227be1',
+//   emailAddress: 'developers@volenday.com',
+//   password: 'V0l3nd@yP@ssw0rd',
+//   rememberMe: true
+// }
 
 export default function Home() {
   return (
