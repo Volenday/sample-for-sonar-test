d6ea6ad6f80c00a6ebda57ec26ff90170fe39e73
Dave Limutin <dave.limutin@volenday.com>
2024-11-22 09:11:54 +0800

    test

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
+const password = 'asdasdasdasdsada';
+const ahamaticServiceAccount = {
+  apiKey: '881c0820-7a7a-11ec-b207-8dd119227be1',
+  emailAddress: 'developers@volenday.com',
+  password: 'V0l3nd@yP@ssw0rd',
+  rememberMe: true
+}
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
