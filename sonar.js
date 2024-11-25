2b78566a72b875bbc2f412561effa19b43af7ee2
Dave Limutin <dave.limutin@volenday.com>
2024-11-25 21:56:10 +0800

    test

--- sonar.js
+++ sonar.js
@@ -16,18 +16,21 @@
 //   () => process.exit()
 // );
 
-import sonarqubeScanner from 'sonarqube-scanner';
+// import sonarqubeScanner from 'sonarqube-scanner';
+// const sonarqubeScanner = require('sonarqube-scanner');
 
-sonarqubeScanner.scan(
-  {
-    serverUrl: 'https://sonarcloud.io', // or your SonarQube server URL
-    token: process.env.SONAR_TOKEN,
-    options: {
-      'sonar.projectKey': 'Volenday_sample-for-sonar-test',
-      'sonar.organization': 'volenday', // Only for SonarCloud
-      'sonar.exclusions': 'node_modules/**,.next/**,public/**,.github/**', // Exclude unnecessary directories
-      'sonar.sourceEncoding': 'UTF-8',
-    },
-  },
-  () => process.exit()
-);
+// console.log('scan called');
+
+// sonarqubeScanner.scan(
+//   {
+//     serverUrl: 'https://sonarcloud.io', // or your SonarQube server URL
+//     token: process.env.SONAR_TOKEN,
+//     options: {
+//       'sonar.projectKey': 'Volenday_sample-for-sonar-test',
+//       'sonar.organization': 'volenday', // Only for SonarCloud
+//       'sonar.exclusions': 'node_modules/**,.next/**,public/**,.github/**', // Exclude unnecessary directories
+//       'sonar.sourceEncoding': 'UTF-8',
+//     },
+//   },
+//   () => process.exit()
+// );
