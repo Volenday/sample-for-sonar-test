baa6bc93ab3b1c5b9c0e0ed15056ae7455382a07
Dave Limutin <dave.limutin@volenday.com>
2024-11-25 20:00:22 +0800

    testing sonar.js

--- sonar.js
+++ sonar.js
@@ -15,3 +15,19 @@
 //   },
 //   () => process.exit()
 // );
+
+import sonarqubeScanner from 'sonarqube-scanner';
+
+sonarqubeScanner.scan(
+  {
+    serverUrl: 'https://sonarcloud.io', // or your SonarQube server URL
+    token: process.env.SONAR_TOKEN,
+    options: {
+      'sonar.projectKey': 'Volenday_sample-for-sonar-test',
+      'sonar.organization': 'volenday', // Only for SonarCloud
+      'sonar.exclusions': 'node_modules/**,.next/**,public/**,.github/**', // Exclude unnecessary directories
+      'sonar.sourceEncoding': 'UTF-8',
+    },
+  },
+  () => process.exit()
+);
